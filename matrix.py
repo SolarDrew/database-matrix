588b0760bd0bf1d35fd6abbd2b05e5dba14435d1
Stuart Mumford <stuart@cadair.com>
2019-07-22 22:55:14 +0100

    Fix up matrix database

--- matrix.py
+++ matrix.py
@@ -1,8 +1,11 @@
 import logging
+from urllib.parse import quote
 
 from matrix_client.errors import MatrixRequestError
 from opsdroid.database import Database
 from opsdroid.connector.matrix.events import MatrixStateEvent
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class DatabaseMatrix(Database):
@@ -13,38 +16,46 @@ class DatabaseMatrix(Database):
         self.name = "matrix"
         self.config = config
         self.room = 'main'
-        logging.debug('Loaded matrix database connector')
+        self._state_key = "opsdroid.database"
+        _LOGGER.debug('Loaded matrix database connector')
 
     async def connect(self, opsdroid):
         """Connect to the database."""
         # Currently can't actually get connectors when this runs, so just store opsdroid instead
         self.opsdroid = opsdroid
-        logging.info("Plugged into the matrix")
+        _LOGGER.info("Plugged into the matrix")
 
     async def put(self, key, data):
         """Insert or replace an object into the database for a given key."""
-        logging.debug(f"Putting {key} into matrix")
         room = self.room
-        room_id = room if room[0] == '!' else conn.room_ids[room]
+        room_id = room if room[0] == '!' else self.connector.room_ids[room]
 
-        await self.opsdroid.send(MatrixStateEvent(key='opsdroid.database',
+        _LOGGER.debug(f"Putting {key} into matrix room {room_id}")
+
+        await self.opsdroid.send(MatrixStateEvent(key=self._state_key,
                                                   content={key: data},
                                                   target=room_id,
                                                   connector=self.connector))
 
     async def get(self, key):
         """Get a document from the database for a given key."""
-        logging.debug(f"Getting {key} from matrix")
         room = self.room
-        room_id = room if room[0] == '!' else conn.room_ids[room]
+        room_id = room if room.startswith('!') else self.connector.room_ids[room]
+
+        _LOGGER.debug(f"Getting {key} from matrix room {room_id}")
 
         try:
-            data = await self.connector.get_state_event(room_id, f'opsdroid.database/{key}')
+            data = await self.get_state_event(room_id)
+            data = data.get(key)
         except MatrixRequestError:
-            data = {}
+            data = None
 
         return data
 
     @property
     def connector(self):
         return self.opsdroid._connector_names['matrix']
+
+    async def get_state_event(self, room_id):
+        return await self.connector.connection._send("GET",
+                                                     quote(f"/rooms/{room_id}/state/{self._state_key}"))
